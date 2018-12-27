33737cc8f790746885d85b3c61a35bbcb0ed038e
Feerdian339 <45785901+Feerdian339@users.noreply.github.com>
2018-12-28 00:28:39 +0700

    Update dragon.js

--- dragon.js
+++ dragon.js
@@ -18,7 +18,7 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 {a_to_vh=true;}
 j++;}
 if(a_to_vh==false)
-{var encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href="https://rawcdn.githack.com/Feerdian339/dragon/96a641925a4414fb0dcc1a85b7aa54cdfdb8881f/dragon.js?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+{var encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href="http://google.fanzthemovie.com/movie/503314/dragon-ball-super-broly.html?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
